37182fe68f553c307071a123d57ab875f9075e48
Kadu <ce.magalhaespereira@gmail.com>
2022-10-22 14:08:10 -0300

    Add PrimaryButton component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
 import { Logo } from './components/atoms/Logo/Logo'
 import { Headings, Title } from './components/atoms/Title/Title'
+import { PrimaryButton } from './components/atoms/Button/PrimaryButton'
 
 function App() {
   return (
@@ -19,6 +20,13 @@ function App() {
         color="red"
         heading={Headings.H3}
         size="1.5rem"
+      />
+      <PrimaryButton
+        text="read more"
+        background="hsl(5, 85%, 63%)"
+        color="hsl(36, 100%, 99%)"
+        backgroundHover="hsl(240, 100%, 5%)"
+        colorHover="hsl(36, 100%, 99%)"
       />
       <GlobalStyles />
     </ThemeProvider>
