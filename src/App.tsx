aa298deb6a9ebbeacc8fb301207e5b3ef0ce95c7
Kadu <ce.magalhaespereira@gmail.com>
2022-10-22 13:23:36 -0300

    Add Title component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
 import { Logo } from './components/atoms/Logo/Logo'
+import { Headings, Title } from './components/atoms/Title/Title'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Logo />
+      <Title
+        text="Lorem ipsum dolor"
+        color="#567890"
+        heading={Headings.H2}
+        size="2rem"
+      />
+      <Title
+        text="This is a title"
+        color="red"
+        heading={Headings.H3}
+        size="1.5rem"
+      />
       <GlobalStyles />
     </ThemeProvider>
   )
