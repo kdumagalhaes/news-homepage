a8ff182368c2c6843ef6b2242f28bb76b113c10a
Kadu <ce.magalhaespereira@gmail.com>
2022-10-26 20:15:03 -0300

    Add molecules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
-import { Logo } from './components/atoms/Logo/Logo'
-import { Headings, Title } from './components/atoms/Title/Title'
-import { PrimaryButton } from './components/atoms/Button/PrimaryButton'
-import { InternalLink } from './components/atoms/Link/InternalLink'
-import { Paragraph } from './components/atoms/Paragraph/Paragraph'
 import { Hero } from './components/molecules/Hero/Hero'
 import { Sidebar } from './components/molecules/Sidebar/Sidebar'
 import { BottomNews } from './components/molecules/BottomNews/BottomNews'
-import { Navigation } from './components/molecules/Navigation/Navigation'
 import { Header } from './components/molecules/Header/Header'
 
 function App() {
